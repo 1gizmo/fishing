5d86d692589355d49b318523d78c0fc923754b59
1gizmo <jdonggwan@naver.com>
2023-06-09 11:29:55 +0900

    fisshing spot

--- src/main/NsFishingSpot.js
+++ src/main/NsFishingSpot.js
@@ -3,12 +3,12 @@ import './scss/NsFishingSpot.scss'
 import ex1 from './img/bg.jpg'
 const NsClass = () => {
   return (
-    <div className='class'>
+    <div className='fishingSpot'>
     <div className='title'>
         <p className='t1'>오늘의 낚시터 > </p>
         <p className='t2'>더보기</p>
     </div>
-    <div className='classs'>
+    <div className='fishingSpots'>
         <div><img src={ex1} /></div>
         <div><img src={ex1} /></div>
         <div><img src={ex1} /></div>
